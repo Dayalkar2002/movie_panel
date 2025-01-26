67e93278fd919b9e5317e208e51ccc278c9a53c5
rutujabhosale24 <rpbhosale24@gmail.com>
2025-01-26 15:40:47 +0530

    App.jsx File

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         
-        <Route path="/movie/:id" element={<MovieDetailPage />} />
         <Route path="/search" element={<SearchPage />} />
       </Routes>
     </Router>
